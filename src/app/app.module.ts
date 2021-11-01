b99776ef3ea0de27b9e4f956dd95cbb0c366fd21
Irma Caceres <irmacmcg116908@gmail.com>
2021-11-01 18:58:32 -0500

    Adding material and doing some events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
+//Librerias de Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+// Componentes del Aplicativo
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatPostComponent } from './creat-post/creat-post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Componentes de Material
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent, CreatPostComponent],
-  imports: [BrowserModule,AppRoutingModule, BrowserAnimationsModule,MatButtonModule],
+  imports: [
+  BrowserModule,
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  FormsModule,
+  MatButtonModule,
+  MatInputModule,
+],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/creat-post/creat-post.component.ts
+++ src/app/creat-post/creat-post.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-creat-post',
+  selector: 'creat-post',
   templateUrl: './creat-post.component.html',
   styleUrls: ['./creat-post.component.css']
 })
 export class CreatPostComponent implements OnInit {
+  content: String = "Hola a todos";
+  texto: string = "";
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showText(){
+  this.content = this.texto;
   }
 
 }
